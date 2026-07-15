be738da4a4d55aff24c237b858e2c82613607ae6
Reinder Roos <rwroos@gmail.com>
2026-07-16 01:40:48 +0200

    Manage Git and Lazygit configuration

--- install.sh
+++ install.sh
@@ -6,14 +6,22 @@ RDE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "RDE installeren vanuit: $RDE_DIR"
 
 mkdir -p "$HOME/.local/scripts"
+mkdir -p "$HOME/.config/lazygit"
 
 ln -sfn "$RDE_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 ln -sfn "$RDE_DIR/scripts/proj" "$HOME/.local/scripts/proj"
+ln -sfn "$RDE_DIR/lazygit/config.yml" "$HOME/.config/lazygit/config.yml"
 chmod +x "$RDE_DIR/scripts/proj"
 
 BASH_LOAD_LINE='if [ -f "$HOME/rde/bash/rde.sh" ]; then source "$HOME/rde/bash/rde.sh"; fi'
 
 touch "$HOME/.bashrc"
+
+if ! git config --global --get-all include.path |
+    grep -Fxq "$RDE_DIR/git/gitconfig"; then
+    git config --global --add include.path \
+        "$RDE_DIR/git/gitconfig"
+fi
 
 if ! grep -Fqx "$BASH_LOAD_LINE" "$HOME/.bashrc"; then
     printf '\n# Load Reinder Development Environment\n%s\n' \
